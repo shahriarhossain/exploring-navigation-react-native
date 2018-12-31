67ca3a35d7f6cc957ebcba553bf5ad6c61314049
shahriar <shahriar_cse@live.com>
2018-12-31 12:36:28 +0600

    creating stack navigator

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+	render() {
+		return <AppContainer />;
+	}
 }
 
+class Home extends React.Component {
+	static navigationOptions = {
+		headerStyle: {
+			backgroundColor: '#eb4d4b'
+		}
+	};
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text>HOME</Text>
+			</View>
+		);
+	}
+}
+
+const AppStackNavigator = createStackNavigator(
+	{
+		Home: Home
+	},
+	{
+		initialRouteName: 'Home'
+	},
+	{
+		defaultNavigationOptions: {
+			headerStyle: {
+				backgroundColor: 'orange'
+			}
+		}
+	}
+);
+
+const AppContainer = createAppContainer(AppStackNavigator);
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
 });
